77c53ca99844cd9a4847427decc28f4a5b7a314b
Patrick Taibel <ptaibel@anexia-it.com>
2022-12-20 15:44:05 +0100

    Fix default middleware driver for JSVM

--- gateway/server.go
+++ gateway/server.go
@@ -783,7 +783,7 @@ func (gw *Gateway) loadCustomMiddleware(spec *APISpec) ([]string, apidef.Middlew
 	mwPostFuncs := []apidef.MiddlewareDefinition{}
 	mwPostKeyAuthFuncs := []apidef.MiddlewareDefinition{}
 	mwResponseFuncs := []apidef.MiddlewareDefinition{}
-	mwDriver := apidef.MiddlewareDriver("")
+	mwDriver := apidef.OttoDriver
 
 	// Set AuthCheck hook
 	if !spec.CustomMiddleware.AuthCheck.Disabled && spec.CustomMiddleware.AuthCheck.Name != "" {
